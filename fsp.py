1c7562b366be0a3ad1436c91498e95ce5fdad9a0
timebotdon <choyyld@gmail.com>
2021-05-15 12:39:17 +0800

    fix  stuff, update functions

--- fsp.py
+++ fsp.py
@@ -3,23 +3,22 @@ import ctypes
 from threading import Thread
 from time import sleep
 
-def showMessage():
+def warn():
     print("Running message..")
     mBox = ctypes.windll.user32.MessageBoxW
     mBox(None, 'Shutdown Imminent! Save your work within the next 10 minutes!', 'Forced Sleep Protocol', 4096)
-    
+
 
 def waitAndShutdown():
     print("Running sleep..")
-    sleep(600000) # 10 Minutes
-    print("Running message..")
-    exec("shutdown /s /t 5")
+    sleep(600) # 10 Minutes
+    exec('shutdown /c "Good Night!" /s /f /t 5')
 
 
 def runtask():
     #Create threads
     t1 = Thread(target=waitAndShutdown, args="")
-    t2 = Thread(target=showMessage, args="")
+    t2 = Thread(target=warn, args="")
 
     # Start threads
     t1.start()
